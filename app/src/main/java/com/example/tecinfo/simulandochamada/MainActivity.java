c5aa365595d7737ebc2d72eb22549db851f916cf
Thiago Sousa Gomes <Thiagosousa8760@gmail.com>
2018-11-12 21:03:07 -0200

    First Commit

---
+++ app/src/main/java/com/example/tecinfo/simulandochamada/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.tecinfo.simulandochamada;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView registrar = (TextView) findViewById(R.id.txt_register);
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        Button login = (Button) findViewById(R.id.btn_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ligacaoActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
