b996addf5ef8315f0397dc5685ab7ce94984f9e9
Thiago Sousa Gomes <Thiagosousa8760@gmail.com>
2018-11-12 22:37:58 -0200

    Terminando tela de login e registrando Dados

--- app/src/main/java/com/example/tecinfo/simulandochamada/MainActivity.java
+++ app/src/main/java/com/example/tecinfo/simulandochamada/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.tecinfo.simulandochamada;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences meusDados;
+    public static final String mypreference = "call";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView registrar = (TextView) findViewById(R.id.txt_register);
+        meusDados = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
 
-        registrar.setOnClickListener(new View.OnClickListener() {
+        final EditText email = findViewById(R.id.edit_email);
+        final EditText senha = findViewById(R.id.edit_senha);
+        Button btnlogin = findViewById(R.id.btn_login);
+        TextView register = findViewById(R.id.txt_register);
+
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
@@ -27,15 +41,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button login = (Button) findViewById(R.id.btn_login);
-
-        login.setOnClickListener(new View.OnClickListener() {
+        btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,ligacaoActivity.class);
-                startActivity(intent);
-                finish();
+
+                String meuEmail = email.getText().toString();
+                String minhaSenha = senha.getText().toString();
+
+                if(meusDados.contains("email") && meusDados.contains("senha")) {
+
+                    if(meuEmail.equals(meusDados.getString("email", ""))
+                            && minhaSenha.equals(meusDados.getString("senha", "")) ) {
+
+                        Intent intent = new Intent(MainActivity.this,
+                                ligacaoActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(),"Email ou senha invalidos!", Toast.LENGTH_LONG).show();
+                    }
+
+
+
+                } else {
+
+                    Toast.makeText(getApplicationContext(),"Não existem cadastros!", Toast.LENGTH_LONG).show();
+
+                }
+
             }
+
         });
+
     }
 }
--- app/src/main/java/com/example/tecinfo/simulandochamada/RegisterActivity.java
+++ app/src/main/java/com/example/tecinfo/simulandochamada/RegisterActivity.java
@@ -1,26 +1,54 @@
 package com.example.tecinfo.simulandochamada;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    SharedPreferences meusDados;
+    public static final String mypreference = "call";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button register = (Button) findViewById(R.id.btn_registrar);
+        meusDados = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
 
-        register.setOnClickListener(new View.OnClickListener() {
+        final EditText novoEmail = findViewById(R.id.edit_r_email);
+        final EditText novaSenha = findViewById(R.id.senha1);
+        final EditText confirmasenha = findViewById(R.id.conf_senha);
+        Button cadastrar = (Button) findViewById(R.id.btn_registrar);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+
+                if (novaSenha.getText().toString().equals(confirmasenha.getText().toString())){
+
+                    SharedPreferences.Editor editor = meusDados.edit();
+                    editor.putString("email", novoEmail.getText().toString());
+                    editor.putString("senha", novaSenha.getText().toString());
+
+                    editor.commit();
+
+                    Toast.makeText(getApplicationContext(),"Dados salvos com sucesso!", Toast.LENGTH_LONG).show();
+
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "As senhas não conferem", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
